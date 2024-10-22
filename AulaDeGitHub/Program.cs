ac217d310b3f77092ca583c6c1dfdfc7a4239e74
Tiago Castro <tiagocastro044@gmail.com>
2024-10-22 16:42:17 +0100

    adição da aula 1

--- AulaDeGitHub/Program.cs
+++ AulaDeGitHub/Program.cs
@@ -12,6 +12,94 @@ namespace AulaDeGitHub
         {
             Console.WriteLine("Hello World!");
 
+            #region PrimeiraParteAula1
+            //Console.Write("Hello World!!!");
+
+            //Console.Write("Hello World 1 !!!");
+            //Console.WriteLine();
+            //Console.WriteLine("\nHello World 2 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+            //Console.WriteLine("\nHello World 77777 !!!");
+
+            ////int 2idade;
+            ////int 2 idade;
+            ////int ´´´´ídáde;
+            //int valorIdade1Pes;
+            //int valorIdade2Pes;
+
+            //uint valorIdade3Pes = 20;
+
+            //char caracter = 'A';
+
+
+
+
+            //Aluno aluno = new Aluno();
+
+            ////Console.Write(aluno.nome ); //Default
+            ////aluno.nome = "Tiago Torres";
+            ////Console.Write(aluno.nome); //Tiago Torres
+
+            //Console.Write("\n {0}", aluno.GetNome());  //Default
+
+            ///*
+            // * 
+            // * Prova de conceito sobre:
+            // * - POO
+            // * - Classes
+            // * - Encapsulamento 
+            // * - Metodos
+            // * 
+            // */
+
+
+            ///* 
+            // aluno.SetNomeQualQuerCoisa("Andre Oliveira");
+            // Console.Write("\n {0}", aluno.GetNome());
+            // aluno.SetNomeQualQuerCoisa("Maria Oliveira");
+            // Console.Write("\n {0}", aluno.GetNome());
+            //*/
+
+
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            #endregion
+
+
+            int x = 10;
+            //x = x + 5;
+            //x += 5;//15
+
+
+
+
+
+            //x = x * 5;
+            //x = x * 5;
+            x *= 5;
+
+            Console.WriteLine(x); //
+
+            Console.ReadKey();
+
             Console.ReadKey();
         }
     }
